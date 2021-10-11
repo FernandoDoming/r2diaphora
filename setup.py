4a93a7aec1b2519d037535b68096b6604fc44bca
Fernando Domínguez <fdominguez@alienvault.com>
2021-10-11 13:02:14 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="r2diaphora",
-    version="0.1.0",
+    version="0.1.1",
     description="radare2 port of diaphora",
     url="https://github.com/FernandoDoming/r2diaphora",
     author="Fernando Domínguez",
